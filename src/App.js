5db4d4129b4620e7f52dc76b36ef93b7faedb62f
Srikrishna <krishna333sp@gmail.com>
2022-12-31 20:50:52 +0530

    url set

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className='App'>
       <BrowserRouter >
         <Routes>
-          <Route index element={<FrontPage />} />
+          <Route path='/*' element={<FrontPage />} />
           <Route path='/beyondoil' element={<ComingSoon />} />
         </Routes>
       </BrowserRouter>
